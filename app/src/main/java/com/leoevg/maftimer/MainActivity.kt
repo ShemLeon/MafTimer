aa762e8e916089e3f49b6e2837a79d03af012a23
ShemLeon <shemiakinleonid@gmail.com>
2025-07-09 23:48:40 +0300

    убрал ошибку разворота

--- app/src/main/java/com/leoevg/maftimer/MainActivity.kt
+++ app/src/main/java/com/leoevg/maftimer/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.ui.Modifier
@@ -26,7 +27,7 @@ class MainActivity : ComponentActivity() {
             HideSystemBars()
 
             MafTimerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { _ -> // Замените innerPadding на _
+                Surface(modifier = Modifier.fillMaxSize()) {
                     MainNavHost(
                         modifier = Modifier.fillMaxSize()
                     )
